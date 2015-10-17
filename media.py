114e27875d1a388abd6db311c9accec67a45ac49
Scott Burns <scott@lentigo.net>
2015-10-17 10:52:23 -0400

    Add Doc Strings to Movie Class
    Add document descriptions to the class and it's methods. Also change the
    argument names for __init__ to be the same as the attribute names.

--- media.py
+++ media.py
@@ -1,17 +1,32 @@
 import webbrowser
 
 class Movie():
-    """This class provides a way to store movie related information"""
+    """Provides Storage for Movie Information.
+
+    This class stores information about a motion picture.
+
+    Attributes:
+        title: The movie's title
+        storyline: A very brief description of the movie_title
+        poster_image_url: The URL of an image of the movie's poster_image
+        trailer_youtube_url: The Youtube URL of the movie's trailer
+        rating: The MPAA assigned movie rating
+
+    Returns:
+        An instance of class Movie
+    """
 
     VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
 
-    def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube, rating):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+    def __init__(self, title, storyline, poster_image_url,
+                 trailer_youtube_url, rating):
+        """Initialize class instance with movie metadata."""
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
         self.rating = rating
 
     def show_trailer(self):
+        """Open a web browser window with the Movie Trailer YouTube URL"""
         webbrowser.open(self.trailer_youtube_url)
